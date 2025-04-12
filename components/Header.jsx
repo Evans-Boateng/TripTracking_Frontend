6c52df63152975bf2b949891dbf9fcc77e682983
Evans <eb5337877@gmail.com>
2025-04-12 04:24:07 +0000

    Implemented the trip form modal successfully

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,21 @@
 import { MdLightMode } from "react-icons/md";
+import { useState } from 'react';
+import { TripForm } from './TripForm';
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="bg-white h-12 w-full border-b-1 border-gray-200 flex items-center px-5 relative">
-      <MdLightMode />
-      <button 
-        className="absolute right-5 bg-blue-600 text-white rounded-md px-3 py-1 hover:bg-blue-500 transition duration-300 ease-in-out shadow-lg focus:outline-blue-600 focus:outline-2 focus:outline-offset-2 cursor-pointer"
-      >
-        Start New Trip
-      </button>
-    </div>
+    <>
+      <div className="bg-white h-12 w-full border-b-1 border-gray-200 flex items-center px-5 relative">
+        <MdLightMode />
+        <button
+          onClick={() => setOpen(true)} 
+          className="absolute right-5 bg-blue-600 text-white rounded-md px-3 py-1 hover:bg-blue-500 transition duration-300 ease-in-out shadow-lg focus:outline-blue-600 focus:outline-2 focus:outline-offset-2 cursor-pointer"
+        >
+          Start New Trip
+        </button>
+      </div>
+      <TripForm open={open} onClose={() => setOpen(false)}/>
+    </>
   )
 };
--- components/TripForm.jsx
+++ components/TripForm.jsx
@@ -1,37 +1,45 @@
 import { Form } from "react-router";
 
-export const TripForm = () => {
+export const TripForm = ({open, onClose}) => {
   return (
-    <div className='bg-white p-5 rounded-lg'>
-      <h1 className="font-[550] mb-3">New Trip Details</h1>
-      <Form className="flex flex-col">
-        <label className="text-sm text-slate-500 font-medium">Current Location</label>
-        <input type="text"
-          placeholder="Enter current location"
-          className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
-        />
-        <label className="text-sm text-slate-500 font-medium">Pickup Location</label>
-        <input type="text"
-          placeholder="Enter pickup location"
-          className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
-        />
-        <label className="text-sm text-slate-500 font-medium">Drop-off Location</label>
-        <input type="text"
-          placeholder="Enter drop-off location"
-          className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
-        />
-        <label className="text-sm text-slate-500 font-medium">Cycle Hours Used</label>
-        <input type="text"
-          placeholder="Enter hours"
-          className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
-        />
-        <button 
-          type="submit"
-          className="bg-blue-600 text-white font-medium text-[15px] py-2 rounded-md w-full cursor-pointer hover:bg-blue-500 transition duration-300 ease-in-out shadow-sm"
-        >
-          Start Trip
-        </button>
-      </Form>
+    <div 
+      onClick={onClose}
+      className={`fixed z-50 inset-0 flex justify-center items-center transition-colors ${open ? "visible bg-black/20" : "hidden"}`}
+    >
+      <div 
+        onClick={event => event.stopPropagation()}
+        className={`bg-white sm:w-[35%] w-[80%] p-5 rounded-lg shadow transition all ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}
+      >
+        <h1 className="font-[550] mb-3">New Trip Details</h1>
+        <Form className="flex flex-col">
+          <label className="text-sm text-slate-500 font-medium">Current Location</label>
+          <input type="text"
+            placeholder="Enter current location"
+            className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
+          />
+          <label className="text-sm text-slate-500 font-medium">Pickup Location</label>
+          <input type="text"
+            placeholder="Enter pickup location"
+            className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
+          />
+          <label className="text-sm text-slate-500 font-medium">Drop-off Location</label>
+          <input type="text"
+            placeholder="Enter drop-off location"
+            className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
+          />
+          <label className="text-sm text-slate-500 font-medium">Cycle Hours Used</label>
+          <input type="text"
+            placeholder="Enter hours"
+            className="border border-gray-200 text-[14px] w-full px-3 py-[8px] focus:border-blue-600 placeholder-slate-400 rounded-md mt-2 mb-4  focus:outline-none focus:ring-[3px] focus:ring-blue-300 focus:ring-opacity-50"
+          />
+          <button 
+            type="submit"
+            className="bg-blue-600 text-white font-medium text-[15px] py-2 rounded-md w-full cursor-pointer hover:bg-blue-500 transition duration-300 ease-in-out shadow-sm"
+          >
+            Start Trip
+          </button>
+        </Form>
+      </div>
     </div>
   )
 };
